b9363aff0689d2250512f5f2609714581261f16e
Suman Chatterjee <chatterj@naf-cms13.desy.de>
2026-05-04 11:30:43 +0200

    a tiny update

--- MVA/run_full_training.sh
+++ MVA/run_full_training.sh
@@ -1,6 +1,6 @@
 source /data/dust/user/chatterj/environments/bdt/bin/activate
 
-python3 Train_BDT_Pairing.py --usemassfraction 1 --fraction_low 0.0  --fraction_high 1.5 
-python3 Train_BDT_Pairing.py --usemassfraction 1 --fraction_low 1.5  --fraction_high 2.5 
-python3 Train_BDT_Pairing.py --usemassfraction 1 --fraction_low 2.5  --fraction_high 3.0 
-python3 Train_BDT_Pairing.py --usemassfraction 1 --fraction_low 3.0  --fraction_high 100.0 
+python3 Train_BDT_Pairing.py --YEAR 2023BPiX --usemassfraction 1 --fraction_low 0.0  --fraction_high 1.5 
+python3 Train_BDT_Pairing.py --YEAR 2023BPiX --usemassfraction 1 --fraction_low 1.5  --fraction_high 2.5 
+python3 Train_BDT_Pairing.py --YEAR 2023BPiX --usemassfraction 1 --fraction_low 2.5  --fraction_high 3.0 
+python3 Train_BDT_Pairing.py --YEAR 2023BPiX --usemassfraction 1 --fraction_low 3.0  --fraction_high 100.0 
